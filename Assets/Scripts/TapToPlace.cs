07c28ab23fce140d2064ad0c2d70479e1bb56e71
Toon <toonschaap3@gmail.com>
2020-06-05 22:54:03 +0200

    Initial commit

---
+++ Assets/Scripts/TapToPlace.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class TapToPlace : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject placeableObject;
+
+    private GameObject spawnObject;
+    private ARRaycastManager _aRRaycastManager;
+    private Vector2 touchPosition;
+
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        _aRRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    bool GetTouchPosition(out Vector2 touchPosition)
+    {
+        if(Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(index: 0).position;
+            return true;
+        }
+
+        touchPosition = default;
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!GetTouchPosition(out Vector2 touchPosition))
+            return;
+
+        if(_aRRaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
+        {
+            var hitPose = hits[0].pose;
+            spawnObject = Instantiate(placeableObject, hitPose.position, hitPose.rotation);
+
+        }
+    }
+}
